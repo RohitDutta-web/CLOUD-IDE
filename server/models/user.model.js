9db4ab263cdfebdabfee43601a925b2392e09a9f
rohit <duttarohit754@gmail.com>
2025-03-30 18:10:09 +0000

    feat(models/user): add user model
    - created user schema with username, email, and guest fields

---
+++ server/models/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  guest: {
+    type: Boolean,
+    required: true,
+    default: false,
+  }
+})
