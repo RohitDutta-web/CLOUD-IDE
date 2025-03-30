1391fca82c92b2ac8d800b8abb68f5af7ac7ac96
rohit <duttarohit754@gmail.com>
2025-03-30 23:34:10 +0530

    compass config

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import {dbConnection} from "./config/db.js";
 dotenv.config({})
 let app = express();
 
@@ -9,4 +10,5 @@ let app = express();
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log("Server is live");
+  dbConnection();
 })
