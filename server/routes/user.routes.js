760563815f0413d52e01d04382923d03b2b670a9
rohit <duttarohit754@gmail.com>
2025-04-12 17:20:57 +0000

    feat(user-routes): Enhance user routes with authentication and logout functionality
    - Added logout route to user routes
    - Implemented middleware for user authentication
    - Updated routes for user registration, login, and email verification.

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,1 +1,13 @@
-import { register, logIn, guestLogIn, updateUser, verifyEmail, sentVerificationMail } from "../controllers/user.controller.js";
+import { register, logIn, guestLogIn, updateUser, verifyEmail, sentVerificationMail, logOut } from "../controllers/user.controller.js";
+import userAuthentication from "../middlewares/auth.middleware.js";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/logIn", logIn);
+router.post("guest/logIn", guestLogIn);
+router.put("/update", userAuthentication,  updateUser);
+router.get("/verification", userAuthentication, sentVerificationMail);
+router.get("/emailVerification", userAuthentication, verifyEmail);
+router.get("/logOut", userAuthentication, logOut);
