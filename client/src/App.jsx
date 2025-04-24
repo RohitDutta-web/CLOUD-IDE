945eb272efc707ea5f7e6c93196773419e9c942b
rohit <duttarohit754@gmail.com>
2025-04-24 18:28:07 +0000

    feat(client): Integrate Tailwind CSS and React Router DOM
    - Added @tailwindcss/vite as a dependency and configured it in vite.config.js
    - Included tailwindcss in index.css
    - Setup routing for user entry page
    - Introduced UserEntry component for user input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
-
+import UserEntry from './pages/userEntry'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
 
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/userEntry' element={<UserEntry />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
