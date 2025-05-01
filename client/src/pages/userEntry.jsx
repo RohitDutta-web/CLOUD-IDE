ffb65c0f396348499f03ff8860aa60a94317a7dd
rohit <duttarohit754@gmail.com>
2025-05-01 15:50:06 +0000

    feat(userEntry): Add user entry UI components
    - Implemented a full-screen layout with flexbox for centering
    - Added Log In and Sign Up buttons with styling
    - Enhanced background and border styling for better visual appeal

--- client/src/pages/userEntry.jsx
+++ client/src/pages/userEntry.jsx
@@ -3,7 +3,22 @@
 export default function UserEntry() {
   return (
     <>
-      
+      <div className="w-screen h-screen flex justify-center items-center bg-zinc-800">
+        <div className="border-2 border-green-700 p-2  h-auto">
+          
+
+          <div className="flex bg-zinc-700 rounded-full justify-between gap-5 p-2 font-bold items-center text-zinc-500">
+            <button className="focus:bg-green-700 focus:text-white rounded-full pt-1 pb-1 pl-2 pr-2 ">Log In</button>
+            <button className="focus:bg-green-700 focus:text-white rounded-full pt-1 pb-1 pl-2 pr-2 ">Sign Up</button>
+         </div>
+
+          <div>
+
+          </div>
+
+        </div>
+        
+     </div>
     </>
   )
 }
