6c4252f8f929d79ce674dfe64607a7c11eadb91c
rohit <duttarohit754@gmail.com>
2025-05-05 20:48:00 +0530

    file system

---
+++ server/controllers/files.controller.js
@@ -0,0 +1,19 @@
+
+
+export const fileCreation = async (req, res) => {
+  try { }
+  catch (e) {}
+}
+
+export const fileDeletion = async(req, res) => {
+  
+}
+
+export const fileDownload = async (req, res) => {
+  
+}
+
+
+export const fileModification = async (req, res) => {
+
+}
