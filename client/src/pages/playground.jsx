382f0bc5f922006379a6107ce0058c444abb6492
rohit <duttarohit754@gmail.com>
2025-05-13 16:36:52 +0000

    feat(playground): add hamburger menu and user icons
    - added 'hamburger-react' dependency to package.json
    - implemented hamburger menu in Playground component
    - display user icons conditionally based on menu state

--- client/src/pages/playground.jsx
+++ client/src/pages/playground.jsx
@@ -1,9 +1,24 @@
-
+import Hamburger from 'hamburger-react'
+import { useState } from 'react'
 
 export default function Playground() {
+  const [isOpen, setOpen] = useState(false)
   return (
     <>
-      
+      <div className="w-screen h-screen bg-zinc-800">
+        <div className='text-white absolute flex flex-col items-center gap-2'>
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+          {
+            isOpen ? null :
+              <>
+                <p className='w-9 h-9 rounded  flex justify-center items-center bg-green-600'>U1</p>
+                <p className='w-9 h-9 rounded  flex justify-center items-center bg-green-600'>U1</p>
+              </>
+          }
+        </div>
+
+
+      </div>
     </>
   )
 }
