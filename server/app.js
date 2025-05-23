3e77f27a2cc2a34ffb8a94e40caa8ab60acb41e4
rohit <duttarohit754@gmail.com>
2025-05-23 22:38:19 +0530

    backend terminal starting

--- server/app.js
+++ server/app.js
@@ -15,10 +15,7 @@ let app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: process.env.CLIENT_URL,
-    credentials: true
-  }
+  cors: '*'
 })
 
 const userSocketMap = new Map();
