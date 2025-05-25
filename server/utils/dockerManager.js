35e8d6cb107c61bf105cfc29d822c6c60932dab2
rohit <duttarohit754@gmail.com>
2025-05-25 15:35:41 +0000

    feat(docker): Add functionality to create a user-specific Docker container
    - Modified createUSerContainer to accept userId
    - Create a user directory for Docker container if it does not exist
    - Configure Docker container with specific bindings and options

--- server/utils/dockerManager.js
+++ server/utils/dockerManager.js
@@ -3,8 +3,22 @@ import fs from "fs";
 
 const docker = new Docker();
 //user private docker container
-export const createUSerContainer = async (userID) => {
-  
+export const createUSerContainer = async (userId) => {
+  const userDir = `/home/codeNimbus/user/${userId}`
+  if (fs.existsSync(userDir)) {
+    fs.mkdirSync(userDir, { recursive: true });
+  }
+
+  const container = await docker.createContainer({
+    Image: "your-sandbox-image",
+    name: `${userId}-codeNimbus-image`,
+    Tty: true,
+    cmd: ['/bin/bash'],
+    HostConfig: {
+      Binds: [`${userDir}:/workspace`],
+      AutoRemove: false,
+    }
+  })
 }
 
 
