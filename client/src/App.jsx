d2ac7bb43a52a0ef2ebc201dbf85ce2b17850b90
rohit <duttarohit754@gmail.com>
2025-05-27 00:46:53 +0530

    room UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import UserEntry from './pages/userEntry'
 import Playground from './pages/playground'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Room from './pages/room'
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/userEntry' element={<UserEntry />} />
         <Route path='/playground' element={<Playground />} />
+        <Route path='/room/:roomId' element={<Room />} />
       </Routes>
 
     </BrowserRouter>
