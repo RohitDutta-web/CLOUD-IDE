d1721fb0b6f69b2da9163af301d45e7fcd57588c
rohit <duttarohit754@gmail.com>
2025-05-26 19:16:53 +0000

    feat(room): Add room page to the application
    - Added Room component for displaying room interface.
    - Updated App component to include route for Room page.

---
+++ client/src/pages/room.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Room() {
+  return (
+    <>
+      <div className='w-full max-w-screen h-screen bg-zinc-800'>
+        <div className='w-full flex '>
+          <textarea className='w-2/3 bg-zinc-700'></textarea>
+          <div className='border-2 w-1/3'>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
