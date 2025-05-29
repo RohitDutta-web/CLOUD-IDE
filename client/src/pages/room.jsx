9355b87d3a18fbb50e3d2fc07babaaa351094898
rohit <duttarohit754@gmail.com>
2025-05-29 18:22:25 +0000

    feat(client): add code editor and terminal features
    - Added @uiw/react-textarea-code-editor for code editing capabilities
    - Integrated xterm.js for terminal functionality
    - Updated dependencies in package.json and package-lock.json
    - Replaced textarea with a CodeEditor component
    - Implemented language selection for the code editor

--- client/src/pages/room.jsx
+++ client/src/pages/room.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import CodeEditor from '@uiw/react-textarea-code-editor';
+import { Terminal } from 'xterm';
+import 'xterm/css/xterm.css';
 
 export default function Room() {
+  const [code, setCode] = useState(
+    `function add(a, b) {\n  return a + b;\n}`
+  );
+
+  const [language, setLanguage] = useState("js")
   return (
     <>
       <div className='w-full max-w-screen h-screen bg-zinc-800'>
-        <div className='w-full flex '>
-          <textarea className='w-2/3 bg-zinc-700'></textarea>
-          <div className='border-2 w-1/3'>
-            <div>
+        <div className='w-full h-2/3 bg-zinc-700'>
+          <CodeEditor
+            value={code}
+            language={language}
+            data-color-mode="dark"
+            placeholder="Please write code."
+            onChange={(evn) => setCode(evn.target.value)}
+            padding={15}
+            style={{
+              backgroundColor: "transparent",
+              paddingTop: 20,
+              fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+            }}
+          />
 
-            </div>
-            <div>
-
-            </div>
-          </div>
         </div>
+        <div id='terminal'></div>
 
       </div>
+
+      <select className=''>
+        <option value="js">JavaScript</option>
+        <option value="c">C</option>
+        <option value="java">Java</option>
+        <option value="python">Python</option>
+        <option value="go">Go</option>
+        <option value="rust">Rust</option>
+        <option value="php">PHP</option>
+        <option value="ruby">Ruby</option>
+        <option value="sql">SQL</option>
+      </select>
     </>
   )
 }
