f0f872b11aebac99cb77f4e67dbecb4baed44e7a
rohit <duttarohit754@gmail.com>
2025-06-02 16:22:43 +0530

    chat box

--- client/src/pages/room.jsx
+++ client/src/pages/room.jsx
@@ -3,11 +3,53 @@ import CodeEditor from '@uiw/react-textarea-code-editor';
 import { IoMdChatboxes } from "react-icons/io";
 import { Terminal } from 'xterm';
 import 'xterm/css/xterm.css';
+import { useParams } from "react-router-dom";
+import { IoMdSend } from "react-icons/io";
 
 export default function Room() {
+
+
+  const messages = [
+    {
+      sender: "Rohit",
+      message: "Hello",
+    },
+      {
+      sender: "Jiju",
+      message: "Hello",
+    },
+        {
+      sender: "Niya",
+      message: "Hello",
+    },
+          {
+      sender: "Baba",
+      message: "Hello",
+    },
+            {
+      sender: "Ma",
+      message: "Hello",
+    },  {
+      sender: "Didi",
+      message: "Hello",
+    },  {
+      sender: "Bubu",
+      message: "Hello",
+    },  {
+      sender: "Attoja",
+      message: "Hello",
+    }
+  ]
+  const { roomId } = useParams();
   const [code, setCode] = useState(
     `function add(a, b) {\n  return a + b;\n}`
   );
+
+  const [chatBox, setChatBox] = useState(false);
+
+  const handleChatBox = () => {
+    setChatBox(!chatBox);
+  }
 
   const handleCodeLan = (e) => {
     const selectedLang = e.target.value;
@@ -67,7 +109,7 @@ export default function Room() {
             }}
           />
           <div className='h-full w-[10%] gap-3 bg-zinc-700 flex flex-col items-center pt-2' id='users' >
-            <p className='text-white'>1124</p>
+            <p className='text-white'>{roomId}</p>
             <div className='flex flex-wrap items-center gap-3 justify-center w-full'>
               <div id='userBox' className=' bg-green-800 font-bold text-white pt-2 pb-2 pl-3 rounded pr-3'>
                 RD
@@ -76,7 +118,7 @@ export default function Room() {
                 RD
               </div>
             </div>
-            <IoMdChatboxes className='cursor-pointer hover:text-white  absolute top-[58%] text-zinc-300 text-4xl'/>
+            <IoMdChatboxes className='cursor-pointer hover:text-white  absolute top-[58%] text-zinc-300 text-4xl'  onClick={handleChatBox} />
 
 
 
@@ -100,9 +142,33 @@ export default function Room() {
       </select>
 
 
-      <div id='chat'>
-
+      {
+        chatBox ? (
+               <div
+      id="chat"
+      className="w-[300px] z-40 absolute top-[20%] right-[8%] rounded shadow-2xl bg-zinc-200 flex flex-col h-[500px]"
+    >
+      
+      <div className="flex-1 overflow-y-auto p-2 space-y-2">
+        {messages.map((_, i) => (
+          <div key={i} className="bg-zinc-400 p-2 rounded">
+            <p className="font-bold text-sm">{ messages[i].sender }</p>
+            <p>{ messages[i].message }</p>
+          </div>
+        ))}
       </div>
+
+    
+      <div className="bg-zinc-700 flex items-center gap-3 p-2">
+        <input
+          type="text"
+          className="bg-zinc-300 w-[80%] rounded-full p-2"
+          placeholder="Type a message..."
+        />
+        <IoMdSend className="text-2xl text-white cursor-pointer" />
+      </div>
+    </div>) : null
+       }
     </>
   )
 }
