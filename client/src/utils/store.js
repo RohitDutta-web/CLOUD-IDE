20166d4b3aa7e42814023f104be98a8822a69895
rohit <duttarohit754@gmail.com>
2025-06-10 23:29:15 +0530

    redux bug fixed

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -2,10 +2,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import expireReducer from 'redux-persist-transform-expire';
+import expireInTransform from "redux-persist-transform-expire-in";
 import userReducer from './userSlice.js';
 
-const userExpireTransform = expireReducer({
+const userExpireTransform = expireInTransform({
   expireKey: import.meta.env.VITE_REDUX_EXPIRE_KEY,
   expireSeconds: 86400,
   autoExpire: true,
