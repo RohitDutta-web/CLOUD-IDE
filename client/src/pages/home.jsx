677f80fd5d44f19a3d204984959f18ab56229ec8
rohit <duttarohit754@gmail.com>
2025-06-19 01:41:30 +0530

    popover btn

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,5 +1,11 @@
 import CircularText from "../assets/animations/circularText"
 import RotatingText from "../assets/animations/rotatingText"
+import { Link } from 'react-router-dom';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 export default function Home() {
   return (
@@ -19,17 +25,17 @@ export default function Home() {
             <div className="flex gap-3 items-center justify-center">
               <p className="font-bold text-green-400 text-4xl">Code</p>
               <RotatingText
-              texts={['Creative', 'Unique', 'on Cloud', 'Everywhere', 'Nimbus']}
-              mainClassName="px-2 w-50 text-4xl font-bold bg-green-400 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
-              staggerFrom={"last"}
-              initial={{ y: "100%" }}
-              animate={{ y: 0 }}
-              exit={{ y: "-120%" }}
-              staggerDuration={0.025}
-              splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-              transition={{ type: "spring", damping: 30, stiffness: 400 }}
-              rotationInterval={2000}
-            />
+                texts={['Creative', 'Unique', 'on Cloud', 'Everywhere', 'Nimbus']}
+                mainClassName="px-2 w-50 text-4xl font-bold bg-green-400 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                staggerFrom={"last"}
+                initial={{ y: "100%" }}
+                animate={{ y: 0 }}
+                exit={{ y: "-120%" }}
+                staggerDuration={0.025}
+                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                rotationInterval={2000}
+              />
             </div>
 
             <div className="text-green-400">
@@ -42,6 +48,26 @@ export default function Home() {
         </div>
 
       </div>
+
+
+
+
+      <Popover  >
+
+        <PopoverTrigger className="absolute hover:outline-2 hover:outline-offset-2 hover:outline-green-300 top-5 right-5 bg-green-700 rounded font-bold text-xl cursor-pointer text-white hover:bg-green-500 pl-7 pr-7 pt-2 pb-2">Room</PopoverTrigger>
+
+        <PopoverContent className="cursor-pointer flex flex-col items-center w-60 justify-center gap-2">
+          <p className="font-bold border-2 w-full border-white hover:border-b-zinc-400 text-center">Create room</p>
+          <p className="font-bold border-2 w-full border-white hover:border-b-zinc-400 text-center">Join room</p>
+        </PopoverContent>
+
+      </Popover>
+
+      <Link to={"/userEntry"} className="absolute hover:outline-2 hover:outline-offset-2 hover:outline-green-300 top-5 right-40 bg-green-700 rounded font-bold text-xl cursor-pointer text-white hover:bg-green-500 pl-7 pr-7 pt-2 pb-2">
+
+        <button className="cursor-pointer">Login / Signup</button>
+
+      </Link>
     </>
   )
 }
