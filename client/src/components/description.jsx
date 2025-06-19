3c00a0882c8d106a9de278c72551935f9063504d
rohit <duttarohit754@gmail.com>
2025-06-19 23:53:42 +0530

    description done

--- client/src/components/description.jsx
+++ client/src/components/description.jsx
@@ -4,7 +4,8 @@ import ScrollFloat from '../assets/animations/floatingText'
 export default function Description() {
   return (
     <>
-      <ScrollFloat
+      <div className="bg-zinc-900 text-green-600 flex w-full font-bold  justify-center">
+        <ScrollFloat
   animationDuration={1}
   ease='back.inOut(2)'
   scrollStart='center bottom+=50%'
@@ -12,8 +13,12 @@ export default function Description() {
         stagger={0.03}
        
 >
-  reactbits
+  Code Nimbus
 </ScrollFloat>
+      </div>
+      <div className='bg-zinc-900 text-green-400 w-full max-w-screen p-10'>
+       <span className='text-green-300 font-bold text-2xl'> Code Nimbus</span> is a next-generation Cloud IDE designed to offer developers a seamless, flexible, and powerful coding experience entirely in the browser. Inspired by the challenges faced in remote development, collaboration, and environment setup, Codenimus aims to eliminate the barriers of traditional local development by providing isolated per-user Docker containers, real-time collaborative editing, integrated terminal access, and direct GitHub integration. Whether you're a student learning to code, a freelancer building projects, or part of a remote team collaborating across time zones, Codenimus empowers you to code, debug, and deploy effortlessly without ever leaving your browser. With features like ephemeral whiteboard rooms for quick sessions and persistent user environments for long-term work, it blends the simplicity of tools like Replit with the power of professional IDEs. Codenimus stands as a meaningful innovation in the cloud development space, making scalable, accessible, and collaborative programming a reality for everyone.
+      </div>
     </>
   )
 }
