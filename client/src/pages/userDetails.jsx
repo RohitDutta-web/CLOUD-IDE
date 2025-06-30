6688c7d2bf8689e6ad17c359d89f031fafab5b08
rohit <duttarohit754@gmail.com>
2025-06-30 17:23:59 +0000

    feat(userDetails): add profile details form with edit icons
    - added a profile details form for user input
    - included icons next to input fields for editing

--- client/src/pages/userDetails.jsx
+++ client/src/pages/userDetails.jsx
@@ -1,9 +1,24 @@
-
+import { FaEdit } from "react-icons/fa";
 
 export default function UserDetails() {
   return (
     <>
-      
+      <div className="w-full max-w-screen ">
+        <p className="w-full text-center pt-10 text-2xl  text-green-400 font-bold pb-10">Profile details</p>
+        <div className="w-full">
+          <form className="bg-zinc-400 w-[50%] h-auto rounded flex flex-col p-5 ">
+            <div >
+              <input type="text" placeholder={"Your user name"} />
+              <FaEdit />
+            </div>
+             <div >
+              <input type="email" placeholder={"Your email id"} />
+              <FaEdit />
+            </div>
+           
+        </form>
+        </div>
+      </div>
     </>
   )
 }
