1323f6ce4c3588dfef2bd4b73617c71a4b4c97eb
rohit <duttarohit754@gmail.com>
2025-07-01 11:17:39 +0530

    Profile details input element UI

--- client/src/pages/userDetails.jsx
+++ client/src/pages/userDetails.jsx
@@ -6,14 +6,14 @@ export default function UserDetails() {
       <div className="w-full max-w-screen ">
         <p className="w-full text-center pt-10 text-2xl  text-green-400 font-bold pb-10">Profile details</p>
         <div className="w-full">
-          <form className="bg-zinc-400 w-[50%] h-auto rounded flex flex-col p-5 ">
-            <div >
-              <input type="text" placeholder={"Your user name"} />
-              <FaEdit />
+          <form className="bg-zinc-400 w-[50%] h-auto rounded flex flex-col p-5 gap-5 ">
+            <div className="flex items-center bg-white opacity-50 p-2 rounded">
+              <input type="text" placeholder={"Your user name"} className="w-[90%] bg-transparent outline-none"/>
+               <FaEdit className="text-4xl p-2 rounded hover:text-white cursor-pointer hover:bg-zinc-800" />
             </div>
-             <div >
-              <input type="email" placeholder={"Your email id"} />
-              <FaEdit />
+            <div className="flex items-center bg-white opacity-50 p-2 rounded">
+              <input type="text" placeholder={"Your email id"} className="w-[90%] bg-transparent outline-none"/>
+              <FaEdit className="text-4xl p-2 rounded hover:text-white cursor-pointer hover:bg-zinc-800" />
             </div>
            
         </form>
