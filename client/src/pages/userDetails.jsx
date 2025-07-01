f06478723bfa8fa8526fc010871744afa53374be
rohit <duttarohit754@gmail.com>
2025-07-01 11:41:24 +0530

    bug arises on Ui

--- client/src/pages/userDetails.jsx
+++ client/src/pages/userDetails.jsx
@@ -1,22 +1,34 @@
 import { FaEdit } from "react-icons/fa";
+import { useState } from "react";
+import { GiConfirmed } from "react-icons/gi";
 
 export default function UserDetails() {
+  const [doEdit, setDoEdit] = useState(false);
+  const handleDoEdit = () => {
+    setDoEdit(!doEdit);
+  }
+
   return (
     <>
       <div className="w-full max-w-screen ">
         <p className="w-full text-center pt-10 text-2xl  text-green-400 font-bold pb-10">Profile details</p>
         <div className="w-full">
           <form className="bg-zinc-400 w-[50%] h-auto rounded flex flex-col p-5 gap-5 ">
-            <div className="flex items-center bg-white opacity-50 p-2 rounded">
-              <input type="text" placeholder={"Your user name"} className="w-[90%] bg-transparent outline-none"/>
-               <FaEdit className="text-4xl p-2 rounded hover:text-white cursor-pointer hover:bg-zinc-800" />
+             {
+                doEdit ?  <GiConfirmed className="text-4xl p-2 rounded hover:text-white cursor-pointer relative  hover:bg-zinc-800"  onClick={handleDoEdit}/>: <FaEdit className="text-4xl p-2 rounded hover:text-white cursor-pointer relative  hover:bg-zinc-800"  onClick={handleDoEdit}/>
+               }
+            
+            <div className="group flex items-center  bg-white opacity-75  group-focus-within:shadow-xl p-2 rounded">
+              <input type="text" placeholder={"Your user name"} className="w-[90%] bg-transparent outline-none p-1 focus:shadow-xl" />
+             
             </div>
-            <div className="flex items-center bg-white opacity-50 p-2 rounded">
-              <input type="text" placeholder={"Your email id"} className="w-[90%] bg-transparent outline-none"/>
-              <FaEdit className="text-4xl p-2 rounded hover:text-white cursor-pointer hover:bg-zinc-800" />
+            <div className="group flex items-center  bg-white opacity-75  group-focus-within:shadow-xl p-2 rounded">
+              <input type="email" placeholder={"Your email id"} className="w-[90%] bg-transparent outline-none p-1 focus:shadow-xl" />
+            
             </div>
-           
-        </form>
+
+          </form>
+          
         </div>
       </div>
     </>
