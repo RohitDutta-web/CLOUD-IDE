861dbdf054ee59eb4c770cb19a0190dfec080164
rohit <duttarohit754@gmail.com>
2025-09-04 06:55:47 +0530

    func( room join function)

--- server/controllers/room.controller.js
+++ server/controllers/room.controller.js
@@ -5,7 +5,7 @@ import { createRoomContainer } from "../utils/dockerManager.js";
 
 //creating room along with its container and enabling with socket
 export const createRoom = async (req, res) => {
-  try { 
+  try {
     const { roomId } = req.body;
     if (!roomId) {
       return res.status(400).json({ message: "provide a valid room id", success: false });
@@ -13,12 +13,12 @@ export const createRoom = async (req, res) => {
     const userId = req.id;
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(400).json({ message: "Please login first" , success: false})
+      return res.status(400).json({ message: "Please login first", success: false })
     }
-    
+
     const container = await createRoomContainer(roomId);
     let existingRoom = await Room.findOne({ roomId })
-    
+
     if (existingRoom) {
       return res.status(400).json({
         message: "Room already running",
@@ -53,8 +53,41 @@ export const createRoom = async (req, res) => {
 //joining room 
 export const joinRoom = async (req, res) => {
   try {
-    
-   }
+    const userId = req.id;
+    const { roomId } = req.body;
+
+    const user = await User.findById(userId)
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid user",
+        success: false
+      })
+    }
+
+    const room = await Room.findOne({ roomId });
+    if (!room) {
+      return res.status(400).json({
+        message: "Invalid room id",
+        success: false
+      })
+    }
+
+    if (!room.users.includes(user._id)) {
+      room.users.push(user._id);
+    }
+
+    await room.save();
+
+
+
+    return res.status(200).json({
+      message: "Room joined",
+      success: true
+    })
+
+
+
+  }
   catch (e) {
     return res.status(400).json({
       message: e.message || "Something went wrong",
